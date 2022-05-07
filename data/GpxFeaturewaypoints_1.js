a87212b6d57937ee00b1366cb5fa116f03d7911c
tnartnik <105103634+tnartnik@users.noreply.github.com>
2022-05-07 14:49:12 +0200

    Prvo nalaganje

---
+++ data/GpxFeaturewaypoints_1.js
@@ -0,0 +1,1 @@
+var json_GpxFeaturewaypoints_1 = {"type":"FeatureCollection","name":"GpxFeaturewaypoints_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
